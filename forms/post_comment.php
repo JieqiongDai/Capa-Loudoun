d253ab009fc077c3ce7f7a37d4344c9193099582
Jieqiong Dai <40248157+JieqiongDai@users.noreply.github.com>
2021-03-21 16:37:25 -0400

    Update post_comment.php

--- forms/post_comment.php
+++ forms/post_comment.php
@@ -1,11 +1,45 @@
 <?php
 require('forms/Persistence.php');
+require('forms/squeeks-Pusher-PHP/lib/Pusher.php');
+require('forms/pusher_config.php');
+
+$ajax = ($_SERVER[ 'HTTP_X_REQUESTED_WITH' ] === 'XMLHttpRequest');
 
 $db = new Persistence();
-if( $db->add_comment($_POST) ) {
-  header( 'Location: index.php' );
+$added = $db->add_comment($_POST);
+
+if($added) {
+  $channel_name = 'comments-' . $added['comment_post_ID'];
+  $event_name = 'new_comment';
+
+  $pusher = new Pusher(APP_KEY, APP_SECRET, APP_ID);
+  $pusher->trigger($channel_name, $event_name, $added);
+}
+
+if($ajax) {
+  sendAjaxResponse($added);
 }
 else {
-  header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
+  sendStandardResponse($added);
+}
+
+function sendAjaxResponse($added) {
+  header("Content-Type: application/json");
+  if($added) {
+    header( 'Status: 201' );
+    echo( json_encode($added) );
+  }
+  else {
+    header( 'Status: 400' );
+  }
+}
+
+function sendStandardResponse($added) {
+  if($added) {
+    header( 'Location: index.php' );
+  }
+  else {
+    header( 'Location: index.php?error=Your comment was not posted due to errors in your form submission' );
+  }
 }
 ?>
